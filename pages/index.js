ca4b8d95dde15e76b452cfef9f5dd1a00ea9eaec
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-04-22 06:55:16 +0200

    Adds favicons and manifest (patch)

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,17 @@ const Index = class extends React.Component {
     return (
       <div>
         <Head>
+          <link rel='apple-touch-icon' sizes='180x180' href='/static/apple-touch-icon.png' />
+          <link rel='icon' type='image/png' sizes='32x32' href='/static/favicon-32x32.png' />
+          <link rel='icon' type='image/png' sizes='16x16' href='/static/favicon-16x16.png' />
+          <link rel='manifest' href='/static/manifest.json' />
+          <link rel='mask-icon' href='/static/safari-pinned-tab.svg' color='#5bbad5' />
+          <link rel='shortcut icon' href='/static/favicon.ico' />
+          <meta name='msapplication-TileColor' content='#da532c' />
+          <meta name='msapplication-config' content='/static/browserconfig.xml' />
+          <meta name='theme-color' content='#ffffff' />
           <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+          <meta name='description' content='Side som genererer tilfeldige ting Eva Susanne elsker.' />
           <title>Drugg gjør livet lysere!</title>
         </Head>
         <div className={'wrapper'}>
